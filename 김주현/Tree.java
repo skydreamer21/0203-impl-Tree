e0255a3a1ad439297f59ef67fad8e157f6e2c0f5
skydreamer21 <skydreamer210@gmail.com>
2023-03-19 19:42:46 +0900

    impl: Tree 인터페이스

---
+++ 김주현/Tree.java
@@ -0,0 +1,12 @@
+public interface Tree {
+    int ROOT = 1;
+    int INIT_SIZE = 10;
+    
+    void addChild(int parent, boolean isLeftChild, int value);
+    void changeRoot(int value);
+    void removeNode(int node);
+    int size();
+    void preOrder();
+    void inOrder();
+    void postOrder();
+}
