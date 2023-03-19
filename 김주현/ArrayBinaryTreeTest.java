9ae6b1bc8b3078e694aad6ba3ade32d537d0400b
skydreamer21 <skydreamer210@gmail.com>
2023-03-19 21:03:49 +0900

    fix: LinkedListBinaryTree 테스트용 인스턴스 추가

--- 김주현/ArrayBinaryTreeTest.java
+++ 김주현/ArrayBinaryTreeTest.java
@@ -15,52 +15,55 @@ public class ArrayBinaryTreeTest {
     static final boolean RIGHT = false;
     
     public static void main(String[] args) {
+        // 주석으로 테스트할 Tree 선택
+        
         // Level : 0
-        ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(1);
+//        ArrayBinaryTree binaryTree = new ArrayBinaryTree(1);
+        LinkedListBinaryTree binaryTree = new LinkedListBinaryTree(1);
         
         // Level : 1
-        arrayBinaryTree.addChild(1, LEFT, 3);
-        arrayBinaryTree.addChild(1, RIGHT, 7);
+        binaryTree.addChild(1, LEFT, 3);
+        binaryTree.addChild(1, RIGHT, 7);
         
         // Level : 2
-        arrayBinaryTree.addChild(2, LEFT, 10);
-        arrayBinaryTree.addChild(2, RIGHT, 5);
-        arrayBinaryTree.addChild(3, RIGHT, 9);
+        binaryTree.addChild(2, LEFT, 10);
+        binaryTree.addChild(2, RIGHT, 5);
+        binaryTree.addChild(3, RIGHT, 9);
         
         // Level : 3
-        arrayBinaryTree.addChild(5, LEFT, 2);
-        arrayBinaryTree.addChild(5, RIGHT, 11);
-        arrayBinaryTree.addChild(7, LEFT, 4);
-        arrayBinaryTree.addChild(7, RIGHT, 6);
+        binaryTree.addChild(5, LEFT, 2);
+        binaryTree.addChild(5, RIGHT, 11);
+        binaryTree.addChild(7, LEFT, 4);
+        binaryTree.addChild(7, RIGHT, 6);
         
         // Level : 4
-        arrayBinaryTree.addChild(15, RIGHT, 20);
+        binaryTree.addChild(15, RIGHT, 20);
         
         // 루트 노드 교체 test
-//        arrayBinaryTree.changeRoot(100);
+//        binaryTree.changeRoot(100);
         
         // Remove test
-//        arrayBinaryTree.removeNode(7);
-//        arrayBinaryTree.addChild(3, RIGHT, 21);
+//        binaryTree.removeNode(7);
+//        binaryTree.addChild(3, RIGHT, 21);
         
         // 없는 부모노드에 자식 추가 test
-//        arrayBinaryTree.addChild(6, RIGHT, 12);
+//        binaryTree.addChild(6, RIGHT, 12);
         
         // 부모노드로 0을 사용 test
-//        arrayBinaryTree.addChild(0, LEFT, 1);
+//        binaryTree.addChild(0, LEFT, 1);
         
         System.out.println("[ PreOrder 순회 ]");
-        arrayBinaryTree.preOrder();
+        binaryTree.preOrder();
         System.out.println("\n");
     
         System.out.println("[ InOrder 순회 ]");
-        arrayBinaryTree.inOrder();
+        binaryTree.inOrder();
         System.out.println("\n");
     
         System.out.println("[ PostOrder 순회 ]");
-        arrayBinaryTree.postOrder();
+        binaryTree.postOrder();
         System.out.println("\n");
     
-        System.out.printf("tree size : %d\n", arrayBinaryTree.size());
+        System.out.printf("tree size : %d\n", binaryTree.size());
     }
 }
